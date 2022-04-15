4b91818459a89007499058c3b9bdc9c9194f44ac
pavelkrivokorytov <paxa-net@mail.ru>
2022-04-15 19:03:19 +0300

    Initial commit

---
+++ src/test/java/pro/sky/lesson/LessonApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.lesson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LessonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
